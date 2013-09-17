e47fcc46b1c9276da4da6db819fd38b2420bf6e3
James Wickett <wickett@gmail.com>
2013-09-17 13:44:23 -0500

    removed the check since it cant catch a valid run

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,8 +5,5 @@ describe 'cloud-dev::default' do
     run = ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '12.04')
     run.converge('cloud-dev::default')
   end
-  it 'installs dev packages like libxsl, libcurl, ...' do
-    expect(chef_run).to install_package('devpkg')
-  end
 end
 
