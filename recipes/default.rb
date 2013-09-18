d0f69184406cfc80e1d3750a85e0d0d70e8348ce
James Wickett <wickett@gmail.com>
2013-09-18 17:38:10 -0500

    working with fonts

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,26 +1,26 @@
 #
-# Cookbook Name:: cloud-dev
-# Recipe:: default
+# cookbook name:: cloud-dev
+# recipe:: default
 #
-# Copyright 2013, James Wickett
+# copyright 2013, james wickett
 #
-# All rights reserved - Do Not Redistribute
+# all rights reserved - do not redistribute
 #
 
-# Default package
+# default package
 include_recipe 'apt' 
 
-%w{ 'libxml2-dev', 'libxsl', 'curl', 'libcurl4-openssl-dev', 'build-essential', 'exuberant-ctags', 'ack' }.each do | dev_pkg |
+%w{libxml2-dev libxslt-dev curl libcurl4-openssl-dev build-essential exuberant-ctags ack xclip}.each do | dev_pkg |
   package dev_pkg do 
     action :install
   end
 end
 
-# Default dev needs
+# default dev needs
 include_recipe 'ntp' 
 include_recipe 'tmux' 
 
-# Ruby 
+# ruby 
 node.override['rvm']['rvm_gem_options'] = "--rdoc --ri"
 node.override['rvm']['user_installs'] = [
     { 'user' => 'ubuntu',
@@ -32,26 +32,26 @@ include_recipe 'rvm::user_install'
 
 bash "get gitub pub keys" do 
   user "ubuntu"
-  code <<-EOH
+  code <<-eoh
     curl https://github.com/wickett.keys 2>/dev/null > /home/ubuntu/.ssh/authorized_keys
     sed -i -e 's@^@command="/usr/bin/tmux -S /tmp/wickett attach -t wickett || /usr/bin/tmux -S /tmp/wickett new -s wickett" @' /home/ubuntu/.ssh/authorized_keys
-    EOH
+    eoh
 end
 
 bash "get tmux conf" do
   user "ubuntu"
-  code <<-EOH
+  code <<-eoh
     curl https://gist.github.com/wickett/6394562/raw/.tmux.conf 2>/dev/null > /home/ubuntu/.tmux.conf
-    EOH
+    eoh
 end
 
 bash "get fonts" do
   user "ubuntu"
-  code <<-EOH
+  code <<-eoh
     mkdir -p /home/ubuntu/.fonts
-    curl https://gist.github.com/qrush/1595572/raw/51bdd743cc1cc551c49457fe1503061b9404183f/Inconsolata-dz-Powerline.otf 2>/dev/null > /home/ubuntu/.fonts/
-    curl https://gist.github.com/qrush/1595572/raw/417a3fa36e35ca91d6d23ac961071094c26e5fad/Menlo-Powerline.otf 2>/dev/null > /home/ubuntu/.fonts/
-    curl https://gist.github.com/qrush/1595572/raw/2eb22321d590265799aac5b166cd19f8358b0db1/mensch-Powerline.otf 2>/dev/null > /home/ubuntu/.fonts/
-    EOH
+    curl https://gist.github.com/qrush/1595572/raw/51bdd743cc1cc551c49457fe1503061b9404183f/inconsolata-dz-powerline.otf 2>/dev/null > /home/ubuntu/.fonts/inconsolata-dz-powerline.otf 
+    curl https://gist.github.com/qrush/1595572/raw/417a3fa36e35ca91d6d23ac961071094c26e5fad/menlo-powerline.otf 2>/dev/null > /home/ubuntu/.fonts/menlo-powerline.otf 
+    curl https://gist.github.com/qrush/1595572/raw/2eb22321d590265799aac5b166cd19f8358b0db1/mensch-Powerline.otf 2>/dev/null > /home/ubuntu/.fonts/mensch-Powerline.otf
+    eoh
 end
 
